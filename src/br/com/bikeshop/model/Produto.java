6daf9b5d1877555dc1feb90081ceb42d812bb372
Thiago Leal <lealthiago435@gmail.com>
2025-11-29 22:05:47 -0300

    refactor(produto): melhorar estrutura e validação da classe Produto

--- src/br/com/bikeshop/model/Produto.java
+++ src/br/com/bikeshop/model/Produto.java
@@ -1,5 +1,65 @@
 package br.com.bikeshop.model;
 
-public abstract class Produto {
+import java.io.Serializable;
 
+public abstract class Produto implements Serializable {
+	private static final long serialVersionUID = 1L; // Boa prática para versão da classe
+	
+	protected String codigo;
+    protected String nome;
+    protected double preco;
+    protected int estoqueAtual;
+    protected int estoqueMinimo; // Cada produto tem o seu alerta
+    
+    // Contructor
+    public Produto(String codigo, String nome, double preco, int estoqueMinimo) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preco = preco;
+		this.estoqueMinimo = estoqueMinimo;
+		this.estoqueAtual = 0;
+	}
+
+
+    // Getters e Setters
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getEstoqueAtual() {
+		return estoqueAtual;
+	}
+
+	public void setEstoqueAtual(int estoqueAtual) {
+		this.estoqueAtual = estoqueAtual;
+	}
+
+	public int getEstoqueMinimo() {
+		return estoqueMinimo;
+	}
+
+	public void setEstoqueMinimo(int estoqueMinimo) {
+		this.estoqueMinimo = estoqueMinimo;
+	}
 }
