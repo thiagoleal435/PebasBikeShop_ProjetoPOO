1cb68c162b89ac1ae2a0e1fc5013deaf5aab4529
Thiago Leal <lealthiago435@gmail.com>
2025-11-29 22:05:36 -0300

    chore(main): ajustar classe Main para inicialização correta do sistema

--- src/br/com/bikeshop/app/Main.java
+++ src/br/com/bikeshop/app/Main.java
@@ -1,10 +1,25 @@
 package br.com.bikeshop.app;
 
+import br.com.bikeshop.controller.ClienteController;
+import br.com.bikeshop.dao.GerenciadorDados;
+import br.com.bikeshop.model.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	        // A tela vai fazer exatamente isso:
+	        ClienteController controller = new ClienteController();
 
-	}
+	        // Simula usuário clicando em salvar
+	        boolean sucesso = controller.salvar("Maria da Silva", "555.555.555-55", "8888-0000");
+	        
+	        if (sucesso) {
+	            System.out.println("A tela mostraria um pop-up de sucesso!");
+	        }
 
-}
+	        // Simula a tabela pedindo dados
+	        System.out.println("Clientes cadastrados: " + controller.listarTodos().size());
+	    }
+    }
