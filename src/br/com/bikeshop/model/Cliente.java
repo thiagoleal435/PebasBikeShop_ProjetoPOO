f76ebae680233a648046c08d3b43ff5b82f7158e
Thiago Leal <lealthiago435@gmail.com>
2025-11-29 22:05:03 -0300

    refactor(cliente): melhorar estrutura da classe Cliente e organização de atributos

--- src/br/com/bikeshop/model/Cliente.java
+++ src/br/com/bikeshop/model/Cliente.java
@@ -1,5 +1,47 @@
 package br.com.bikeshop.model;
 
-public class Cliente {
+import java.io.Serializable;
 
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    // Constructor
+    public Cliente(String nome, String cpf, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+    
+    // Getters and Setters
+    public String toString() {
+        return "Cliente: " + nome + " (CPF: " + cpf + ")";
+    }
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
